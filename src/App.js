9aa3a92575ac24ec5530d790b0557e5522e0f40f
Haroldo Carvalho <harold@MacBook-Pro-de-Haroldo.local>
2022-02-02 18:43:53 -0300

    Primeira tela

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import { ProductsList } from './views/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main-app">
+      <h1>Amazing Store</h1>
+      <BrowserRouter>
+        <Route exact path="/" component={ProductsList} />
+      </BrowserRouter>
     </div>
   );
 }
